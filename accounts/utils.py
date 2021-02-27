3cd5af58a3ca853f8f2dcd14df55ac8abd1c5425
smrtsilver <strongsnoop@gmail.com>
2021-02-27 14:38:21 +0330

    send sms

--- accounts/utils.py
+++ accounts/utils.py
@@ -9,11 +9,11 @@ def sendsmsmethod(number, format=None):
 
         a = sms.objects.update_or_create(phonenumber=number)
         time_otp = a[0].key
-        # re = smspanel(number, time_otp)
+        re = smspanel(number, time_otp)
         content = {
             "message": "کد تایید شما در نرم افزار {} می باشد".format(time_otp),
             "success": True,
-            # "smssend": re
+            "smssend": re
         }
         # smspanel(number, time_otp)
         return content
@@ -22,15 +22,14 @@ def sendsmsmethod(number, format=None):
 def smspanel(number, smscode):
     data=re.match(r'^09(3|0)[0-9]{8}$',number)
 
-    baseUrl = "http://185.112.33.62/api/v1/rest/sms/"
+    baseUrl = "http://185.112.33.62/api/v1/rest/sms/send"
     custom_header = {"token": "b93502c9d8661db7a3ee956220e439d12f5d2d43"}
     kh_num="90003561"
     omo_num="10001000000321"
-    basejson = {"from": kh_num,"recipients":[number,],"message":smscode,"type":0,"patternID": 50,"authcode":smscode}
+    message=f"کد تایید شما در برنامه شهر من : {smscode}"
+    basejson = {"from": kh_num,"recipients":[number,],"message":message,"type":0}
     if data:
         basejson["from"]=omo_num
-        basejson["message"]=f"{smscode}کد تایید شما در برنامه شهرمن :"
-        basejson.pop("authcode")
     else:
         basejson["from"]=kh_num
     request = requests.post(baseUrl, json=basejson, headers=custom_header)
