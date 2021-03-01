e5dff2714951c0acc4e613766d948a382a157fbe
smrtsilver <strongsnoop@gmail.com>
2021-03-01 16:40:06 +0330

    edit admin panel for khabarnegar !

--- content/admin.py
+++ content/admin.py
@@ -5,7 +5,7 @@ from content.models import *
 from nested_admin.nested import NestedModelAdmin, NestedStackedInline, NestedTabularInline
 
 # admin.site.register(base_content)
-admin.site.register(group)
+# admin.site.register(group)
 # admin.site.register(tariff)
 # admin.site.register(city_prob)
 # admin.site.register(Image)
@@ -48,22 +48,36 @@ class basecontentAdmin(NestedModelAdmin):
     list_display = ['title', 'valid', "group", "view", "call"]
     exclude = ("NOTSHOW","author")
     ordering = ['valid',"-create_time"]
-    list_filter = ("valid","group")
-    list_filter = (
-        'author',
-         "valid",
-        "group"
-    )
+    # list_filter = ("valid","group")
+    list_filter = [
+        "city",
+    ]
+
+    def changelist_view(self, request, extra_context=None):
+        if request.user.is_superuser:
+            self.list_filter=[
+                    "city",
+                        ]
+            self.list_filter.extend(["valid"])
+            self.list_filter.extend(["group"])
+
+        return super(basecontentAdmin, self).changelist_view(request, extra_context)
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        form.base_fields['phonenumber'].initial = request.user.username
+        if not is_superuser:
+            form.base_fields['phonenumber'].disabled = True
+
+            if request.user.groups.filter(id=1).exists():
+                form.base_fields["group"].disabled = True
+                form.base_fields['group'].initial = 1
+
+        return form
+
     readonly_fields = [
         'createtime',
     ]
-    #
-    # def get_call(self, obj):
-    #     return obj.log_content.filter(action=1).count()
-    # get_call.admin_order_field = 'log_content'
-    # def get_view(self, obj):
-    #     return obj.log_content.filter(action=2).count()
-    # get_view.admin_order_field = 'log_content'
 
     def createtime(self,obj):
         time=obj.get_time()
